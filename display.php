f5951307616787b5ac0fe7aac5ae97cb53371b4f
Subhadippal006 <subhadippal006@gmail.com>
2022-01-19 11:55:17 +0530

    added new buttn

--- display.php
+++ display.php
@@ -11,9 +11,79 @@
 
  <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
    <script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
-
+   
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    
+     figcaption {
+      color:black;
+      font-style: italic;
+      padding: 5px;
+      text-align: center;
+    }
+    * {
+      box-sizing: border-box;
+    }
+    
+    .column {
+      float: left;
+      width: 20%;
+      padding: 20px;
+    }
+    
+    /* Clearfix (clear floats) */
+    .row::after {
+      content: "";
+      clear: both;
+      display: table;
+    }
+    </style>
+<style>
+  
+   ul {
+     list-style-type: none;
+     margin: 0;
+     padding: 0;
+     overflow:hidden;
+     background-color: #FFD700;
+   }
+   
+   li {
+     float: left;
+   }
+   
+   li a {
+     display: block;
+     color: black;
+     text-align: center;
+     padding: 14px 16px;
+     font-size: large;
+     text-decoration: none;
+   }
+   
+   li a:hover {
+     background-color:ghostwhite;
+   }
+   .checked {
+  color: orange;
+}
+ body{
+               background-color:ghostwhite;
+            } 
+			
+   </style>
+   
+  
 </head>
 <body>
+<ul>
+   <li><a class="active" href="home.html"><b>Home</b></a></li>
+   <li><a href="couriers.html"><b>Couriers</b></a></li>
+   <li><a href="staff.html"><b>Staff</b></a></li>
+   <li><a href="schedule.html"><b>Schedule</b></a></li>
+   <li><a href="delivery.html"><b>Delivery</b></a></li>
+ </ul>
+ <br>
 
  <div class="container">
  <div class="col-lg-12">
