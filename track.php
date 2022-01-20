f8cd5adb5c5479ca9474f02761d4d4883195c7f7
Subhadippal006 <subhadippal006@gmail.com>
2022-01-20 08:49:25 +0530

    final changes

--- track.php
+++ track.php
@@ -5,58 +5,8 @@
 table, th, td {
     border: 1px solid black;
 }
-figcaption {
-      color:black;
-      font-style: italic;
-      padding: 5px;
-      text-align: center;
-    }
-    * {
-      box-sizing: border-box;
-    }
+
     
-    .column {
-      float: left;
-      width: 20%;
-      padding: 20px;
-    }
-    
-    /* Clearfix (clear floats) */
-    .row::after {
-      content: "";
-      clear: both;
-      display: table;
-    }
-    </style>
-<style>
-  
-   ul {
-     list-style-type: none;
-     margin: 0;
-     padding: 0;
-     overflow:hidden;
-     background-color: #FFD700;
-   }
-   
-   li {
-     float: left;
-   }
-   
-   li a {
-     display: block;
-     color: black;
-     text-align: center;
-     padding: 14px 16px;
-     font-size: large;
-     text-decoration: none;
-   }
-   
-   li a:hover {
-     background-color:ghostwhite;
-   }
-   .checked {
-  color: orange;
-}
 div {
   border: 10px solid black;
   background-color: ghostwhite;
@@ -68,28 +18,12 @@ div {
  body{
                background-color:ghostwhite;
             } 
-			.button {
-  background-color:gold;
-  border: none;
-  color: black;
-  padding: 4px 15px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
+		
 </style>
 <body>
-<ul>
-   <li><a class="active" href="home.html"><b>Home</b></a></li>
-   <li><a href="couriers.html"><b>Courier</b></a></li>
-   <li><a href="staff.html"><b>Staff</b></a></li>
-   <li><a href="schedule.html"><b>Schedule</b></a></li>
-   <li><a href="delivery.html"><b>Delivery</b></a></li>
- </ul>
+<br>
+<br>
+<br>
 <center>
 <h1>Courier Details</h1>
 </center>
